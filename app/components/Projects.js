673af1c8d64ad5c8488cb0b2000e1b2e2ab06fad
Timon Kayser <timonkayser@icloud.com>
2025-02-05 19:38:52 +0100

    Added array for projects for modularity

--- app/components/Projects.js
+++ app/components/Projects.js
@@ -1,4 +1,25 @@
 // components/Projects.js
+const projectsData = [
+    {
+        id: 1,
+        imageSrc: "images/encoder.png",
+        altText: "Encoder",
+        description: "Encoder",
+    },
+    {
+        id: 2,
+        imageSrc: "images/encoder.png",
+        altText: "Project 2",
+        description: "Test2",
+    },
+    {
+        id: 3,
+        imageSrc: "images/encoder.png",
+        altText: "Project 3",
+        description: "Test3",
+    },
+];
+
 export default function Projects() {
     return (
         <section id="projects" className="w-full min-h-96 flex items-start justify-center px-4 bg-dunkelgrau">
@@ -9,12 +30,15 @@ export default function Projects() {
                 <p className="text-white text-center text-opacity-30 text-xs">
                     Used languages: C, C++, Python, Next.js, Java
                 </p>
-                <div className="flex flex-wrap gap-4 justify-center pt-6">
-                    <img src="images/encoder.png" alt="En/Decoder" className="h-48" />
-                    <img src="images/encoder.png" alt="Project 2" className="h-48" />
-                    <img src="images/encoder.png" alt="Project 3" className="h-48" />
+                <div className="flex flex-wrap gap-8 justify-center pt-6">
+                    {projectsData.map((project) => (
+                        <div key={project.id} className="flex flex-col items-center">
+                            <img src={project.imageSrc} alt={project.altText} className="h-48" />
+                            <p className="text-white text-base mt-2">{project.description}</p> 
+                        </div>   
+                    ))}
                 </div>
-                
+
             </div>
         </section>
     );
